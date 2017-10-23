76962fe41d66b5795bce7e1b5add53df49dd4909
lucas <lucas.kl369@gmail.com>
2017-10-23 03:34:44 +0200

    Initial Commit

---
+++ SpeechTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpeechTest
+//
+//  Created by Mac on 10/23/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpeechTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpeechTest
+//
+//  Created by Mac on 10/23/17.
+//  Copyright © 2017 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
